bee360d2d06f71ec9d2916f65d6117dd3efc37eb
Vanessa Schneider <vanessajwade@gmail.com>
2022-01-05 17:48:49 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your code here!
+
+
+
+main.remove();
+
+
+
+
+
+const victory = document.querySelector("victory");
+const newHeader = document.createElement("h1") 
+newHeader.id = "victory"
+newHeader.textContent = "YOUR-NAME is the champion";
+victory.append(h1);
+
+
+
